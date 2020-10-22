dc757e8093d5c3909198ef10395bdc87f6685c89
naumenko.roman2003 <naumenko.roman2003@gmail.com>
2020-10-22 10:53:55 +0300

    Прибрав зайві дужки у формулі для визначення двугого катета.

--- Lab1_v27/Lab1_v27.py
+++ Lab1_v27/Lab1_v27.py
@@ -4,6 +4,6 @@ a = float(input("Enter first leg: "))
 if c <= a or (c <= 0 or a <= 0):
     print("This triangle doesn't exist!")
 else:
-    b = ((c) ** 2 - (a) ** 2) ** 0.5
+    b = (c ** 2 - a ** 2) ** 0.5
     r = (a + b - c) / 2
     print("The radius of the inscribed circle:", r)
