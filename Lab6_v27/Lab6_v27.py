424ba1bfee569133d536f809806c96a83a74957d
naumenko.roman2003 <naumenko.roman2003@gmail.com>
2020-12-01 21:32:51 +0200

    +Lab_6

---
+++ Lab6_v27/Lab6_v27.py
@@ -0,0 +1,13 @@
+from math import pi
+
+def min_value(a, b):
+    return b if a <= b else a
+
+a = int(input("Enter a: "))
+b = int(input("Enter b: "))
+
+U = min_value(a, b)
+V = min_value(a * b, a + b)
+W = min_value(U + V ** 2, pi)
+
+print(U, V, W)
